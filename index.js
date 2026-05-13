3943e989b4f923a23c47a49f82d440531f0c142e
Md Fuad Amir <mdfuadamir@gmail.com>
2026-05-13 06:44:32 +0600

    added live url

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://aura-design-tan.vercel.app"],
     credentials: true,
   }),
 );
